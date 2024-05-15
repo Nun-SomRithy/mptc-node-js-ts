d3cdf7799fcce5346cf1392c822bb763a1d0fb18
nunsomrithy098 <nunsomrithy098@gmail.com>
2024-05-15 17:00:02 +0700

    controller

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
+// Import express, cors, helmet and morgan
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import router from './routes';
 
 // Create Express server
 const app = express(); // New express instance
@@ -11,16 +13,16 @@ const port = 3000; // Port number
 app.use(cors()); // Enable CORS
 app.use(helmet()); // Enable Helmet
 app.use(morgan('dev')); // Enable Morgan
+app.use(express.json()); // <=== Enable JSON body parser
+
+// Use routes
+app.use('/', router);
 
 // Start Express server
 app.listen(port, () => {
   // Callback function when server is successfully started
   console.log(`Server started at http://localhost:${port}`);
 });
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
 
 // Export Express app
 export default app;
